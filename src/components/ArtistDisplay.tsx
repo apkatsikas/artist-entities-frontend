d017970a549aed89dd0f9895bab0164d729ab5fd
apkatsikas <apkatsikas@gmail.com>
2025-12-29 20:10:18 -0500

    Add spacing

--- src/components/ArtistDisplay.tsx
+++ src/components/ArtistDisplay.tsx
@@ -4,7 +4,13 @@ type ArtistDisplayProps = {
 
 function ArtistDisplay({ artist }: ArtistDisplayProps) {
   return (
-    <div className="horiz-center white-text" id="artist">
+    <div
+      className="horiz-center white-text"
+      id="artist"
+      style={{
+        minHeight: 'clamp(3rem, 25vw, 15rem)',
+      }}
+    >
       {artist}
     </div>
   );
